eff96815d37a9bccaeebaba576e158611c072c0b
Gizemlimsn <148919696+Gizemlimsn@users.noreply.github.com>
2024-11-28 10:47:03 +0300

    Update App.java

--- App.java
+++ App.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int mat,fizik,kimya,turkce,tarih,muzik,ort;
+        String sonuc;
+        Scanner Input=new Scanner(System.in);
+        System.out.print("Matematik notunu giriniz: ");
+        mat=Input.nextInt();
+        System.out.print("Fizik notunu giriniz: ");
+        fizik=Input.nextInt();
+        System.out.print("Kimya notunu giriniz: ");
+        kimya=Input.nextInt();
+        System.out.print("Türkçe notunu giriniz: ");
+        turkce=Input.nextInt();
+        System.out.print("Tarih notunu giriniz: ");
+        tarih=Input.nextInt();
+        System.out.print("Müzik notunu giriniz: ");
+        muzik=Input.nextInt();
+        ort=(mat+fizik+kimya+turkce+tarih+muzik)/6;
+        System.out.println("Ortalama: "+ort);
+        sonuc=(ort>60)?"Geçti":"Kaldı";
+        System.out.println(sonuc);
     }
 }
