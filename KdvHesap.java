194810f14b448372f42f6ead12c7ef2c75de0b69
Gizem SEN <sengizemsen96@gmail.com>
2024-12-04 08:26:44 +0300

    Kdv Oranları Değiştirildi.

--- KdvHesap.java
+++ KdvHesap.java
@@ -6,8 +6,8 @@ public class KdvHesap {
         Scanner Input=new Scanner(System.in);
         System.out.println("Tutarı giriniz");
         tutar=Input.nextDouble();
-        oran=(tutar>1000)?8:18;
-        Kdv=(tutar*oran)/100;
+        oran=(tutar>1000)?0.8:0.18;
+        Kdv=tutar*oran;
         tutar=tutar+Kdv;
         System.out.println("KDV: "+Kdv);
         System.out.println("Hesaplanan tutar: "+tutar);
